753892c581ff2d18dc3776422461e7d2b3b73e0a
Angel <anyaga@andrew.cmu.edu>
2024-09-13 13:01:47 -0400

    inheritance integer list update

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -6,11 +6,35 @@ package edu.cmu.cs.cs214.rec04;
  * with the current size, which goes down when an element is removed)
  * and exports an accessor (totalAdded) for this count.
  *
- * @author Nora Shoemaker
+ * @author Angel Nyaga
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
 
+    public InheritanceSortedIntList(){
+        super(); //Check!!!! 
+        this.totalAdd = 0;
+    }
+
+    @Override
+    boolean add(int newNum){
+        boolean addResult = super.add(newNum);
+        if(addResult){
+            this.totalAdd++;
+            return true;
+        } return false;
+    }
+
+    @Override
+    boolean addAll(IntegerList list){
+        boolean addAllResult = super.addAll(list);
+        if(addAllResult) return true;
+        return false;
+    }
+
+    int getTotalAdded(){
+        return this.totalAdd;
+    }
 }
